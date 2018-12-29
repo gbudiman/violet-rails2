e7d8d8cc89d9fbfa791170b46d801c79ab2e873e
Gloria Budiman <wahyu.g@gmail.com>
2018-12-29 05:52:31 -0800

    Monkey-patched OpenStruct spec

---
+++ spec/lib/ext/open_struct_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe OpenStruct, type: :monkey_patch do
+  subject do
+    OpenStruct.new(
+      a: true,
+      b: true,
+      c: false
+    )
+  end
+
+
+  it { expect(subject.has_all?(:a, :b)).to eq true }
+  it { expect(subject.has_all?(:a, :b, :c)).to eq false }
+  it { expect(subject.has_all?(:a, :b, :c, :d)).to eq false }
+  it { expect(subject.has_one?(:b, :c)).to eq true }
+  it { expect(subject.has_one?(:b, :c, :d)).to eq true }
+end
