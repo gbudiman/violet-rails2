e1a72fa32a605a0addfcefc2f6ad673f073fecad
Gloria Budiman <wahyu.g@gmail.com>
2018-12-29 00:47:17 -0800

    Basic test post-rubocop

--- app/models/battlefield.rb
+++ app/models/battlefield.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class Battlefield < ApplicationRecord
+  after_initialize :generate_seed
+  has_many :agents, dependent: :destroy
+  validates :seed, presence: true
+
+  private
+
+    def generate_seed
+      self.seed = Random.new.seed % 2**63
+    end
 end
