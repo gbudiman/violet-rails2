c196d09b9fbc76801d4e82439d106d7f392b758f
Gloria Budiman <wahyu.g@gmail.com>
2018-12-31 13:45:21 -0800

    Rubocop and Codecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+
+require "codecov"
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
