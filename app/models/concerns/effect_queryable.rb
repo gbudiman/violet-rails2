f0213ecf25aae95d53483e40f621a36720ef7efc
Gloria Budiman <wahyu.g@gmail.com>
2019-01-01 03:16:02 -0800

    Complete EffectQueryable module

--- app/models/concerns/effect_queryable.rb
+++ app/models/concerns/effect_queryable.rb
@@ -12,5 +12,27 @@ module Concerns
 
       false
     end
+
+    def -(arg)
+      if self[:stack].present? && self[:stack].is_a?(Integer)
+        self[:stack] = [0, self[:stack] - arg].max
+      elsif self[:duration].present?
+        self[:duration] = [0, self[:duration] - arg].max
+      end
+
+      self
+    end
+
+    def tick!
+      self - 1
+    end
+
+    def duration
+      self[:duration]
+    end
+
+    def stack
+      self[:stack]
+    end
   end
 end
--- spec/models/concerns/effect_queryable.rb
+++ spec/models/concerns/effect_queryable.rb
@@ -26,4 +26,73 @@ RSpec.describe Concerns::EffectQueryable, type: :concern do
       it "expired durational effect" do subject.efx = { duration: 0 } end
     end
   end
+
+  context "#-" do
+    context "on permanent effect" do
+      it "should stay permanent" do
+        subject.efx = { stack: :permanent }
+        subject.efx - 1
+        expect(subject.efx).to eq(stack: :permanent)
+      end
+    end
+
+    context "on stacked effect" do
+      before { subject.efx = { stack: 12 } }
+      it "should reduce the stack accordingly" do
+        subject.efx - 8
+        expect(subject.efx).to eq(stack: 4)
+      end
+
+      it "should not reduce the stack below 0" do
+        subject.efx - 25
+        expect(subject.efx).to eq(stack: 0)
+      end
+    end
+
+    context "on durational effect" do
+      before { subject.efx = { duration: 30 } }
+      it "should reduce the duration accordingly" do
+        subject.efx - 15.1
+        expect(subject.efx.duration).to be_within(0.2).of(14.9)
+      end
+
+      it "should not reduce the duration below 0" do
+        subject.efx - 49.4
+        expect(subject.efx).to eq(duration: 0)
+      end
+    end
+  end
+
+  context "#tick" do
+    context "on permanent effect" do
+      it "should stay permanent" do
+        subject.efx = { stack: :permanent }
+        expect(subject.efx.tick!).to eq(stack: :permanent)
+      end
+    end
+
+    context "on stacked effect" do
+      it "should reduce the stack accordingly" do
+        subject.efx = { stack: 12 }
+        expect(subject.efx.tick!).to eq(stack: 11)
+      end
+
+      it 'should not reduce the stack below 0' do
+        subject.efx = { stack: 0 }
+        expect(subject.efx.tick!).to eq(stack: 0)
+      end
+    end
+
+    context "on durational effect" do
+      it "should reduce the duration accordingly" do
+        subject.efx = { duration: 12.3 }
+        expect(subject.efx.tick!.duration).to be_within(0.2).of(11.3)
+      end
+
+      it "should not reduce the duration below 0" do
+        subject.efx = { duration: 0.1}
+        expect(subject.efx.tick!.duration).to eq(0)
+      end
+    end
+  end
 end
