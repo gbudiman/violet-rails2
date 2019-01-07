88e06b162e14f01290168fed4a92914382036f22
Gloria Budiman <wahyu.g@gmail.com>
2019-01-06 22:02:12 -0800

    Baseline equippable spec

--- app/models/concerns/equippable.rb
+++ app/models/concerns/equippable.rb
@@ -3,10 +3,70 @@ module Concerns
     extend ActiveSupport
 
     def self.extended(base)
-      Concerns::Anatomiable::VALID_ANATOMIES.each do |anatomy|
+      (Concerns::Anatomiable::VALID_ANATOMIES).each do |anatomy|
+        define_method(anatomy) do
+          EquippableProxy.new(self, anatomy)
+        end
+
+        define_method("#{anatomy}=") do |value|
+          self[anatomy] = value.extend(EquipQueryable)
+
+          value.each do |prop, propval|
+            self[anatomy].define!(prop, propval)
+          end
+        end
       end
 
       Concerns::Weaponizable::VALID_WEAPONIZABLE.each do |anatomy|
+      end
+    end
+
+    def import!(h)
+      h.each do |k, v|
+        self.send("#{k}=", v)
+      end
+
+      self
+    end
+
+    class EquippableProxy
+      #attr_reader :target
+      #delegate_missing_to :target
+
+      def initialize(ancestor, attribute)
+        @ancestor = ancestor
+        @attribute = attribute
+        @target = @ancestor[@attribute]
+      end
+
+      def available?
+        @target.is_a?(Hash)
+      end
+
+      def equippable?
+        !@target.nil? && @target.blank?
+      end
+
+      def method_missing(m, *args)
+        @target.send(m)
+      end
+    end
+
+    module EquipQueryable
+      def define! prop, values
+        case prop
+        when :props
+          values.each do |value|
+            define_singleton_method("#{value}?") { true }
+          end
+        when :weight
+          self[prop] = values
+          define_singleton_method(prop) { self[prop] }
+        end
+      end
+
+      def method_missing(m, *args)
+        return false
       end
     end
   end
---
+++ spec/models/concerns/equippable_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Concerns::Equippable, type: :model do
+  subject { {}.extend(Concerns::Equippable) }
+
+  context "assignment" do
+    let(:input) do
+      {
+        hand_main: { props: [:sword, :steel], weight: 20 },
+        hand_off: { props: [:shield, :wooden], weight: 18 },
+        arm_main: {},
+      }
+    end
+
+    before { subject.import!(input) }
+
+    it "should store object properties correctly" do
+      expect(subject.hand_main.available?).to eq true
+      expect(subject.hand_main.equippable?).to eq false
+      expect(subject.hand_main.sword?).to eq true
+      expect(subject.hand_main.steel?).to eq true
+      expect(subject.hand_main.throwable?).to eq false
+      expect(subject.hand_main.weight).to eq input[:hand_main][:weight]
+      expect(subject.arm_main.available?).to eq true
+      expect(subject.arm_main.equippable?).to eq true
+      expect(subject.arm_off.available?).to eq false
+      expect(subject.arm_off.equippable?).to eq false
+    end
+  end
+end
