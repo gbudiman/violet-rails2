245d6e2bc8c05424ac2d980765d26c548b961a77
Gloria Budiman <wahyu.g@gmail.com>
2019-01-23 23:22:32 -0800

    Proper Coverage generation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,13 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
+
+require "simplecov"
+SimpleCov.start
+
+require "codecov"
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
